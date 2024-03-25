428177eea5be6de48fd3a20437bf7cf6e510028f
ggoma <freddie@gameround.co>
2024-03-25 14:51:38 +0900

    Initialize Commit

---
+++ src/test/java/com/freehw/FreehwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freehw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreehwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
